da34d5f995681800130fdaecbbe23cec4ab6a80d
Taki Guan <taki.guan@gmail.com>
2021-02-04 23:01:33 +0800

    perf: :zap: user HyperlinkedModelSerializer replace some ModelSerializer

--- backend/apps/lists/serializers.py
+++ backend/apps/lists/serializers.py
@@ -6,11 +6,11 @@ Date: 2021-02-04 21:05:55
 LastEditors: Taki Guan
 LastEditTime: 2021-02-04 21:05:58
 """
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import HyperlinkedModelSerializer
 from .models import List
 
 
-class ListSerializer(ModelSerializer):
+class ListSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = List
         fields = "__all__"
--- backend/apps/tasks/serializers.py
+++ backend/apps/tasks/serializers.py
@@ -4,13 +4,13 @@ Version: 1
 Author: Taki Guan
 Date: 2021-02-03 22:22:15
 LastEditors: Taki Guan
-LastEditTime: 2021-02-03 22:22:17
+LastEditTime: 2021-02-04 22:59:39
 """
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import HyperlinkedModelSerializer
 from .models import Task
 
 
-class TaskSerializer(ModelSerializer):
+class TaskSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Task
         fields = "__all__"
