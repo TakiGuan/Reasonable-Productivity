849a850ded9c893fdb05cb6d6cc32c01e9e06caa
Taki Guan <taki.guan@gmail.com>
2021-02-07 14:20:18 +0800

    chore: :wrench: add CORS header configuration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # 3rd Party
     "rest_framework",
-    "rest_framework_nested",
+    "corsheaders",
     # User Apps
     "apps.tasks",
     "apps.users",
@@ -50,12 +50,16 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    # CORS Should Place Before
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
 
 AUTH_USER_MODEL = "users.User"
 
