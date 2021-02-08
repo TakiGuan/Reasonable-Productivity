abbd0fb25943c018677486a61960819cc0bfa37d
Taki Guan <taki.guan@gmail.com>
2021-02-08 08:09:14 +0800

    feat: :sparkles: add completed and date field to task model

--- backend/apps/tasks/models.py
+++ backend/apps/tasks/models.py
@@ -4,7 +4,7 @@ Version: 1
 Author: Taki Guan
 Date: 2021-02-03 21:40:02
 LastEditors: Taki Guan
-LastEditTime: 2021-02-04 21:20:00
+LastEditTime: 2021-02-07 16:59:13
 """
 from django.db import models
 from django.contrib.auth import get_user_model
@@ -16,8 +16,8 @@ class Task(Timestamp, models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=1000, blank=True)
-    # created_at = models.DateTimeField(auto_now_add=True)
-    # updated_at = models.DateTimeField(auto_now=True)
+    completed = models.BooleanField(default=False)
+    date = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.title
